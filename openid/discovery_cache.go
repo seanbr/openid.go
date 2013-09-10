cfac0d9442d76f27254b7f898076a660cfedcc75
Sean Browning <seanbrowning@gmail.com>
2013-09-09 23:53:09 -0700

    Openid with Redis for nonces and discovery cache

--- openid/discovery_cache.go
+++ openid/discovery_cache.go
@@ -1,52 +1,78 @@
 package openid
 
+import "github.com/garyburd/redigo/redis"
+import "log"
+
 type DiscoveredInfo interface {
-  OpEndpoint() string
-  OpLocalId() string
-  ClaimedId() string
-  // ProtocolVersion: it's always openId 2.
+	OpEndpoint() string
+	OpLocalId() string
+	ClaimedId() string
+	// ProtocolVersion: it's always openId 2.
 }
 
 type DiscoveryCache interface {
-  Put(id string, info DiscoveredInfo)
-  // Return a discovered info, or nil.
-  Get(id string) DiscoveredInfo
+	Put(id string, info DiscoveredInfo)
+	// Return a discovered info, or nil.
+	Get(id string) DiscoveredInfo
 }
 
-type SimpleDiscoveredInfo struct {
-  opEndpoint string
-  opLocalId  string
-  claimedId  string
+//type SimpleDiscoveredInfo struct {
+//opEndpoint string
+//opLocalId  string
+//claimedId  string
+//}
+
+//func (s *SimpleDiscoveredInfo) OpEndpoint() string {
+//return s.opEndpoint
+//}
+
+//func (s *SimpleDiscoveredInfo) OpLocalId() string {
+//return s.opLocalId
+//}
+
+//func (s *SimpleDiscoveredInfo) ClaimedId() string {
+//return s.claimedId
+//}
+
+type RedisDiscoveryCache struct {
+	Pool *redis.Pool
 }
 
-func (s *SimpleDiscoveredInfo) OpEndpoint() string {
-  return s.opEndpoint
+type RedisDiscoveredInfo struct {
+	opEndpoint string
+	opLocalId  string
+	claimedId  string
 }
 
-func (s *SimpleDiscoveredInfo) OpLocalId() string {
-  return s.opLocalId
+func (s *RedisDiscoveredInfo) OpEndpoint() string {
+	return s.opEndpoint
 }
 
-func (s *SimpleDiscoveredInfo) ClaimedId() string {
-  return s.claimedId
+func (s *RedisDiscoveredInfo) OpLocalId() string {
+	return s.opLocalId
 }
 
-type SimpleDiscoveryCache map[string]DiscoveredInfo
-
-func (s SimpleDiscoveryCache) Put(id string, info DiscoveredInfo) {
-  s[id] = info
+func (s *RedisDiscoveredInfo) ClaimedId() string {
+	return s.claimedId
 }
 
-func (s SimpleDiscoveryCache) Get(id string) DiscoveredInfo {
-  if info, has := s[id]; has {
-    return info
-  }
-  return nil
+func (s RedisDiscoveryCache) Put(id string, info DiscoveredInfo) {
+	log.Print("--------------PUT--------------")
+}
+
+func (s RedisDiscoveryCache) Get(id string) DiscoveredInfo {
+	info, err := s.Pool.Get().Do("GET", "steam_discovery_cache:"+id)
+	if info != nil {
+		log.Print(info)
+		//return info
+	}
+	log.Print(err)
+	return nil
 }
 
 func compareDiscoveredInfo(a DiscoveredInfo, opEndpoint, opLocalId, claimedId string) bool {
-  return a != nil &&
-    a.OpEndpoint() == opEndpoint &&
-    a.OpLocalId() == opLocalId &&
-    a.ClaimedId() == claimedId
+	return a != nil &&
+		a.OpEndpoint() == opEndpoint &&
+		a.OpLocalId() == opLocalId &&
+		a.ClaimedId() == claimedId
 }
--- openid/nonce_store.go
+++ openid/nonce_store.go
@@ -1,85 +1,55 @@
 package openid
 
 import (
-  "errors"
-  "flag"
-  "fmt"
-  "sync"
-  "time"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"time"
 )
 
 var max_nonce_age = flag.Duration("openid-max-nonce-age",
-  60*time.Second,
-  "Maximum accepted age for openid nonces. The bigger, the more"+
-    "memory is needed to store used nonces.")
-
-var nonceStore = &SimpleNonceStore{Store: make(map[string][]*Nonce)}
+	60*time.Second,
+	"Maximum accepted age for openid nonces. The bigger, the more"+
+		"memory is needed to store used nonces.")
 
 type NonceStore interface {
-  // Returns nil if accepted, an error otherwise.
-  Accept(endpoint, nonce string) error
+	Accept(endpoint, nonce string) error
 }
 
 type Nonce struct {
-  T time.Time
-  S string
+	T time.Time
+	S string
 }
 
-type SimpleNonceStore struct {
-  Store map[string][]*Nonce
-  mutex sync.Mutex
+type RedisNonceStore struct {
+	Pool *redis.Pool
 }
 
-func (d *SimpleNonceStore) Accept(endpoint, nonce string) error {
-  // Value: A string 255 characters or less in length, that MUST be
-  // unique to this particular successful authentication response.
-  if len(nonce) < 20 || len(nonce) > 256 {
-    return errors.New("Invalid nonce")
-  }
+func (store *RedisNonceStore) Accept(endpoint, nonce string) error {
+	if len(nonce) < 20 || len(nonce) > 256 {
+		return errors.New("Invalid nonce")
+	}
 
-  // The nonce MUST start with the current time on the server, and MAY
-  // contain additional ASCII characters in the range 33-126 inclusive
-  // (printable non-whitespace characters), as necessary to make each
-  // response unique. The date and time MUST be formatted as specified in
-  // section 5.6 of [RFC3339], with the following restrictions:
+	ts, err := time.Parse(time.RFC3339, nonce[0:20])
+	if err != nil {
+		return err
+	}
 
-  // All times must be in the UTC timezone, indicated with a "Z".  No
-  // fractional seconds are allowed For example:
-  // 2005-05-15T17:11:51ZUNIQUE
-  ts, err := time.Parse(time.RFC3339, nonce[0:20])
-  if err != nil {
-    return err
-  }
-  now := time.Now()
-  diff := now.Sub(ts)
-  if diff > *max_nonce_age {
-    return fmt.Errorf("Nonce too old: %ds", diff.Seconds())
-  }
+	now := time.Now()
+	diff := now.Sub(ts)
+	if diff > *max_nonce_age {
+		return fmt.Errorf("Nonce too old: %ds", diff.Seconds())
+	}
 
-  s := nonce[20:]
-
-  // Meh.. now we have to use a mutex, to protect that map from
-  // concurrent access. Could put a go routine in charge of it
-  // though.
-  d.mutex.Lock()
-  defer d.mutex.Unlock()
-
-  if nonces, hasOp := d.Store[endpoint]; hasOp {
-    // Delete old nonces while we are at it.
-    newNonces := []*Nonce{}
-    for _, n := range nonces {
-      if n.T == ts && n.S == s {
-        // If return early, just ignore the filtered list
-        // we have been building so far...
-        return errors.New("Nonce already used")
-      }
-      if now.Sub(n.T) < *max_nonce_age {
-        newNonces = append(newNonces, n)
-      }
-    }
-    d.Store[endpoint] = newNonces
-  } else {
-    d.Store[endpoint] = []*Nonce{{ts, s}}
-  }
-  return nil
+	s := nonce[20:]
+	_, err = store.Pool.Get().Do("SET", "steam_nonces:"+ts.String(), s)
+	if err != nil {
+		return err
+	}
+	_, err = store.Pool.Get().Do("EXPIRE", "steam_nonces:"+ts.String(), 60)
+	if err != nil {
+		return err
+	}
+	return nil
 }
